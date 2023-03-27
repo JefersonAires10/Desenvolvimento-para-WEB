d89d507e41e1ee9ea6bfc8b6fb9d7cc9d0bfc987
Jeferson Aires <111990809+JefersonAires10@users.noreply.github.com>
2023-03-26 22:03:11 -0300

    00Atividade

---
+++ introducao/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+//import HelloWorld from './components/HelloWorld';
+//import Calculadora from './components/01Calculadora';
+//import MyProps from './components/02MyProps';
+//import MeusDados from './components/01MeusDados';
+//import MeusDados from './components/atividade00/02MeusDados';
+import Temperatura from './components/atividade00/03Temperatura';
+
+
+function App() {
+  return (
+    <Temperatura
+    />
+  );
+}
+
+/*function App() {
+  return (
+    <MeusDados 
+    nome = "Jeferson Aires de Sousa"
+    curso = "Ciência da Commputação"
+    universidade = "UFC"
+    />
+  );
+}*/
+
+/*function App() {
+  return (
+    <div className="App">
+      <MeusDados />
+    </div>
+  );
+}*/
+
+/*function App() {
+  return (
+    <div className="App">
+      <Site.Header />
+      <Site.Body />
+      <Site.Footer />
+    </div>
+  );
+}*/
+
+export default App;
